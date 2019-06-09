2e37c5d2e8b772e947495f4e8dd9cf5a1671e871
ryanty <42381062+ryanYtan@users.noreply.github.com>
2019-06-09 18:43:45 +0800

    Added more logic and CSS styling to the webpage.
    - added a bit of CSS styling to the calorie counter
    
    - added some text to the bottom of the counter
    
    - did some logic of obtaining the data from the form... Currently it stores it in a FormData object, and you can iterate over it using formData.entries(). So far I haven't done any returning
    
    - added the logic of adding and removing rows from the table

--- website/static/scripts.js
+++ website/static/scripts.js
@@ -1,7 +1,39 @@
-$(document).ready(function() {
+let nRows = 0; // current number of rows
 
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
-    });
+function appendRow(tableID) {
+  var table = document.getElementsByClassName(tableID)[0];
+  var newRow = table.insertRow(-1);
+  nRows++;
+  const input1 = "<input type='text' name='ingredient-" + nRows + "' class='input=ingredient'>";
+  const input2 = "<input type='number' name='amount-" + nRows + "' min='0' class='input-amount'>";
+  const input3 = "<input type='text' name='unit-" + nRows + "' class='input-unit'>";
+  var cell1 = newRow.insertCell(0);
+  var cell2 = newRow.insertCell(1);
+  var cell3 = newRow.insertCell(2);
+  cell1.innerHTML = input1;
+  cell2.innerHTML = input2;
+  cell3.innerHTML = input3;
+}
 
+function deleteLastRow(tableID) {
+  if (nRows > 0) {
+    nRows--;
+    const table = document.getElementsByClassName(tableID)[0];
+    table.deleteRow(-1);
+  }
+}
+
+function getInput() {
+  const str = new FormData(document.forms["calorie-input"]);
+  document.forms["calorie-input"].submit();
+  for (var pair of str.entries()) {
+    alert(pair[0] + ', ' + pair[1]);
+  }
+}
+
+
+$(document).ready(function () {
+  $('#sidebarCollapse').on('click', function () {
+    $('#sidebar').toggleClass('active');
+  });
 });
