634801fecf88d2c432c9dfa76d37a8ac443aaa16
Kevin Lim <lyskevin@gmail.com>
2019-06-17 23:09:36 +0800

    Added new routes (daily_goals, graph_generator, settings and about) and changed recipe to calorie_counter

--- website/application.py
+++ website/application.py
@@ -6,9 +6,25 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/recipe")
-def home():
-    return render_template("recipe.html")
+@app.route("/calorie_counter")
+def calorie_counter():
+    return render_template("calorie_counter.html")
+
+@app.route("/daily_goals")
+def daily_goals():
+    return render_template("daily_goals.html")
+
+@app.route("/graph_generator")
+def graph_generator():
+    return render_template("graph_generator.html")
+
+@app.route("/settings")
+def settings():
+    return render_template("settings.html")
+
+@app.route("/about")
+def abotu():
+    return render_template("about.html")
 
 @app.route("/contact")
 def contact():
