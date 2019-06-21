4f6b7c99af2a2d51f62edd88457c266716e249e9
Kevin Lim <lyskevin@gmail.com>
2019-06-21 10:36:21 +0800

    Modified table_create.py to work with the new food.csv file

--- website/table_create.py
+++ website/table_create.py
@@ -1,15 +1,14 @@
-# Has to be run in the static directory
-
 import csv, sqlite3
 
 connection = sqlite3.connect("food.db")
 cursor = connection.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS food (householdWeight1 REAL, householdWeight1Description VARCHAR(255), householdWeight2 REAL, householdWeight2Description VARCHAR(255), description VARCHAR(255));")
+cursor.execute("CREATE TABLE IF NOT EXISTS food (description VARCHAR(255), weightInGrams REAL, measure VARCHAR(255), energyPerMeasure INTEGER);")
 
-with open('food.csv', 'r') as file_in:
+with open('download.csv', 'r') as file_in:
     dict_reader = csv.DictReader(file_in)
-    to_db = [(column['1st Household Weight'], column['1st Household Weight Description'], column['2nd Household Weight'], column['2nd Household Weight Description'], column['Description']) for column in dict_reader]
+    to_db = [(column['Description'], column['Weight(g)'], column['Measure'], column['Energy(kcal)Per Measure']) for column in dict_reader]
 
-cursor.executemany("INSERT INTO food (householdWeight1, householdWeight1Description, householdWeight2, householdWeight2Description, description) VALUES (?, ?, ?, ?, ?);", to_db)
+cursor.executemany("INSERT INTO food (description, weightInGrams, measure, energyPerMeasure) VALUES (?, ?, ?, ?);", to_db)
 connection.commit()
 connection.close()
+
