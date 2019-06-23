cc49358f3071d0861f507fe52dd1fdb6b9b9da4c
Kevin Lim <lyskevin@gmail.com>
2019-06-23 18:19:10 +0800

    Changed unit selection code from <input> to <select> and added onclick events when an ingredient is slected from the autocomplete options

--- website/static/scripts.js
+++ website/static/scripts.js
@@ -1,22 +1,32 @@
-let nRows = 0; // current number of rows
+let nRows = 1; // current number of rows
 
 function appendRow(tableID) {
   var table = document.getElementsByClassName(tableID)[0];
   var newRow = table.insertRow(-1);
   nRows++;
-  const input1 = "<input type='text' name='ingredient-" + nRows + "' class='input=ingredient'>";
-  const input2 = "<input type='number' name='amount-" + nRows + "' min='0' class='input-amount'>";
-  const input3 = "<input type='text' name='unit-" + nRows + "' class='input-unit'>";
+  const input1 = "<div class=\"input\" id=\"ingredient\">\n"
+    + "  <input type=\"text\" id=\"ingredient-" + nRows + "\" "
+    + "class=\"input-ingredient typeahead\" placeholder=\"Enter Ingredient\">\n"
+    + "</div>";
+  const input2 = "<div class=\"input\">\n"
+    + "  <input type=\"number\" id=\"amount-" + nRows + "\" min=\"0\" "
+    + "class=\"input-amount\" placeholder=\"Enter Amount\">\n"
+    + "</div>";
+  const input3 = "<div class=\"input\">\n"
+    + "  <select type=\"text\" id=\"unit-" + nRows + "\" "
+    + "class=\"input-unit\" placeholder=\"Choose Unit\">\n"
+    + "</div>";
   var cell1 = newRow.insertCell(0);
   var cell2 = newRow.insertCell(1);
   var cell3 = newRow.insertCell(2);
   cell1.innerHTML = input1;
   cell2.innerHTML = input2;
   cell3.innerHTML = input3;
+  configure();
 }
 
 function deleteLastRow(tableID) {
-  if (nRows > 0) {
+  if (nRows > 1) {
     nRows--;
     const table = document.getElementsByClassName(tableID)[0];
     table.deleteRow(-1);
@@ -33,10 +43,17 @@ function getInput() {
 
 // Execute when the DOM is fully loaded
 $(document).ready(function() {
+  
   $('#sidebarCollapse').on('click', function() {
     $('#sidebar').toggleClass('active');
   });
 
+  configure();
+
+});
+
+function configure() {
+  
   // Configure typeahead
   $('#ingredient .typeahead').typeahead({
     minLength: 1,
@@ -51,15 +68,16 @@ $(document).ready(function() {
   });
 
   // Retrieve nutritional information after ingredient is selected
-  $("#ingredient .typeahead").bind("typeahead:select", function(event, suggestion) {
-    alert(suggestion["description"]);
+  $("#ingredient .typeahead").on("typeahead:select", function(event, suggestion) {
+
+    // Configure units
+    var unitNumber = this.id.split("-")[1];
+    var units = document.getElementById("unit-" + unitNumber);
+    units.options[0] = new Option("grams (g)", "grams");
+
   });
 
-/*
-  // Clear input field
-  $("#ingredient .typeahead").typeahead("setQuery", "");
-*/
-});
+}
 
 // Search database for typeahead's suggestions
 function search(query, syncResults, asyncResults) {
