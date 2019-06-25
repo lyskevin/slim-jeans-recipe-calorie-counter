58ef86bf1a9ff857669f163a7ab5ac141ffd3460
Kevin Lim <lyskevin@gmail.com>
2019-06-25 13:32:19 +0800

    Added code to check that all input fields are filled in before a new row can be added

--- website/static/scripts.js
+++ website/static/scripts.js
@@ -2,28 +2,35 @@ var nRows = 1; // current number of rows
 var ingredientInformation = {};
 
 function appendRow(tableID) {
-  var table = document.getElementsByClassName(tableID)[0];
-  var newRow = table.insertRow(-1);
-  nRows++;
-  const input1 = "<div class=\"input\" id=\"ingredient\">\n"
-    + "  <input type=\"text\" id=\"ingredient-" + nRows + "\" "
-    + "class=\"input-ingredient typeahead\" placeholder=\"Enter Ingredient\">\n"
-    + "</div>";
-  const input2 = "<div class=\"input\">\n"
-    + "  <input type=\"number\" id=\"amount-" + nRows + "\" min=\"0\" "
-    + "class=\"input-amount\" placeholder=\"Enter Amount\">\n"
-    + "</div>";
-  const input3 = "<div class=\"input\">\n"
-    + "  <select type=\"text\" id=\"unit-" + nRows + "\" "
-    + "class=\"input-unit\" placeholder=\"Choose Unit\">\n"
-    + "</div>";
-  var cell1 = newRow.insertCell(0);
-  var cell2 = newRow.insertCell(1);
-  var cell3 = newRow.insertCell(2);
-  cell1.innerHTML = input1;
-  cell2.innerHTML = input2;
-  cell3.innerHTML = input3;
-  configure();
+  var table = document.getElementById("input-table");
+  //var table = document.getElementsByClassName(tableID)[0];
+  if ($("#ingredient-1").val() == ""
+      || $("#amount-1").val() == ""
+      || $("#unit-1").val() == "") {
+    alert("Please fill in all input fields for the previous ingredient");
+  } else {
+    var newRow = table.insertRow(-1);
+    nRows++;
+    const input1 = "<div class=\"input\" id=\"ingredient\">\n"
+      + "  <input type=\"text\" id=\"ingredient-" + nRows + "\" "
+      + "class=\"input-ingredient typeahead\" placeholder=\"Enter Ingredient\">\n"
+      + "</div>";
+    const input2 = "<div class=\"input\">\n"
+      + "  <input type=\"number\" id=\"amount-" + nRows + "\" min=\"0\" "
+      + "class=\"input-amount\" placeholder=\"Enter Amount\">\n"
+      + "</div>";
+    const input3 = "<div class=\"input\">\n"
+      + "  <select type=\"text\" id=\"unit-" + nRows + "\" "
+      + "class=\"input-unit\" placeholder=\"Choose Unit\">\n"
+      + "</div>";
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+    var cell3 = newRow.insertCell(2);
+    cell1.innerHTML = input1;
+    cell2.innerHTML = input2;
+    cell3.innerHTML = input3;
+    configure();
+  }
 }
 
 function deleteLastRow(tableID) {
@@ -35,24 +42,19 @@ function deleteLastRow(tableID) {
 }
 
 function getInput() {
-
   var form = document.forms["calorie-input"];
   for (var i = 0; i < form.length - 4; i += 3) {
     alert("You have selected " + form[i + 1].value + " " + form[i + 2].value
           + " of " + form[i].value);
   }
-
 }
 
 // Execute when the DOM is fully loaded
-$(document).ready(function() {
-  
+$(document).ready(function() {  
   $('#sidebarCollapse').on('click', function() {
     $('#sidebar').toggleClass('active');
   });
-
   configure();
-
 });
 
 function configure() {
