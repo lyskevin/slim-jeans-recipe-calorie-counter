23893f7f442e99cd7ebb04fdfc166a47c14b0a9b
Kevin Lim <lyskevin@gmail.com>
2019-06-30 23:29:28 +0800

    Modified table_create.py to reference food.csv instead of download.csv

--- website/table_create.py
+++ website/table_create.py
@@ -4,7 +4,7 @@ connection = sqlite3.connect("food.db")
 cursor = connection.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS food (description VARCHAR(255), weightInGrams REAL, measure VARCHAR(255), energyPerMeasure INTEGER);")
 
-with open('download.csv', 'r') as file_in:
+with open('food.csv', 'r') as file_in:
     dict_reader = csv.DictReader(file_in)
     to_db = [(column['Description'], column['Weight(g)'], column['Measure'], column['Energy(kcal)Per Measure']) for column in dict_reader]
 
