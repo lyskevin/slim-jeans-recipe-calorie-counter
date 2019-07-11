ef4bba13dd54dfa9b7955c10266f37324d001480
ryanYtan <tanyuryan@gmail.com>
2019-07-11 14:13:01 +0800

    Combined about.html and contact.html into about page.

--- website/application.py
+++ website/application.py
@@ -26,12 +26,8 @@ def settings():
     return render_template("settings.html")
 
 @app.route("/about")
-def abotu():
+def about():
     return render_template("about.html")
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
 
 @app.route("/search")
 def search():
